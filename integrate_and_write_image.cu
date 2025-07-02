5ddab9ad7dc12d4ca64328c522d9407773268746
hanaizakim <123072120+hanaizakim@users.noreply.github.com>
2025-07-02 12:42:46 +0200

    Working with separated function

--- integrate_and_write_image.cu
+++ integrate_and_write_image.cu
@@ -49,6 +49,17 @@ struct Vec3 {
 
 };
 
+__host__ __device__ float density(const Vec3 x){
+  const Vec3 c(0.0f, 0.0f, 0.0f);
+  const float R = 0.5f;
+  Vec3 dx = x - c;
+  float r = dx.length();
+  if (dx.length() < R){
+    return 1.0f;
+  }
+  return 0.0f;
+}
+
 class Object{
   public:
   __host__ __device__ virtual float density(const Vec3 x) const = 0;
@@ -84,7 +95,7 @@ struct Cube : Object{
 // Integrate the density along the ray from the origin to the end point.
 // Hierarchical integration method which is more efficient than simple integration.
 // Refines the integration step size based on the density of the scene.
-__host__ __device__ float integrate_hierarchical(const Object& object, const Vec3 origin, const Vec3 _direction, float DS, float smin, float smax){
+__host__ __device__ float integrate_hierarchical(const Vec3 origin, const Vec3 _direction, float DS, float smin, float smax){
 	Vec3 direction = _direction.normalize();
 	// integrate using sliding window
 	float right = smin + DS;
@@ -94,12 +105,12 @@ __host__ __device__ float integrate_hierarchical(const Object& object, const Vec
 	float T = 0.0f; //flat_field;
 	while (right <= smax){
     Vec3 x = origin + direction * right;
-		float rho = object.density(x);
+		float rho = density(x);
 		if ((rho == 0) != (prev_rho == 0)){ // rho changed between left and right
 			left += ds;
 			while (left < right){
         x = origin + direction * left;
-				T += object.density(x) * ds;
+				T += density(x) * ds;
 				left += ds;
 			}
 			T += rho * ds; // reuse rho from right
@@ -115,8 +126,7 @@ __host__ __device__ float integrate_hierarchical(const Object& object, const Vec
 
 __global__ void assemble_image_kernel(
   float* output,
-  int width,
-  const Object& object
+  int width
   ){
   int i = blockIdx.x * blockDim.x + threadIdx.x;
   int j = blockIdx.y * blockDim.y + threadIdx.y;
@@ -128,7 +138,7 @@ __global__ void assemble_image_kernel(
   Vec3 x0(-3.0, y, z);
   Vec3 x1(3.0, y, z);
   Vec3 t = x1 - x0;
-  output[i*width + j] = integrate_hierarchical(object, x0, t, 0.01f, 0.0f, 6.0f);
+  output[i*width + j] = integrate_hierarchical(x0, t, 0.01f, 0.0f, 6.0f);
   return;
 }
 
